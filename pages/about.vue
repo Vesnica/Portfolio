79629894c4f67649307fa986d8e892a296bd8370
Vesnica <Vesnica@users.noreply.github.com>
2018-03-13 13:56:26 +0800

    Initial Commit

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">About Me</h1>
+      <p>
+        My name is Vesnica, I’m a self taught software developer based in Shanghai, China. I build modern applications for the web! I'm a technical writer, I write technical articles and tutorials for various platforms including
+        <a href="https://vesnica.com">Vesnica.com</a>.
+      </p>
+    </div>
+  </section>
+</template>
